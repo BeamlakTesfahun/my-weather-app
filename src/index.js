db144b7803c6f8ea5aa2474319beca2149a9fb9f
Beamlak <beamlakt24@gmail.com>
2023-11-05 11:47:51 +0300

    Injected more weather parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let dateTimeSelector = document.querySelector("#date-time");
+let day = now.getDay();
+let hour = now.getHours();
+let min = now.getMinutes();
+if (hour < 10) {
+  hour = "0" + hour;
+}
+if (min < 10) {
+  min = "0" + min;
+}
+dateTimeSelector.innerHTML = `${days[day]} ${hour}:${min}`;
+
 function updateWeatherParameters(response) {
   console.log(response.data);
   let citySelector = document.querySelector("#city-id");
@@ -6,6 +28,13 @@ function updateWeatherParameters(response) {
   let tempSelector = document.querySelector("#temp-id");
   console.log(tempSelector);
   tempSelector.innerHTML = Math.round(temp);
+  let humSelector = document.querySelector("#humidity-id");
+  let windSpeedSelector = document.querySelector("#speed-id");
+  let decriptionSeelctor = document.querySelector("#description-id");
+  console.log(decriptionSeelctor);
+  humSelector.innerHTML = response.data.main.humidity;
+  windSpeedSelector.innerHTML = Math.round(response.data.wind.speed);
+  decriptionSeelctor.innerHTML = response.data.weather[0].description;
 }
 function cityFunction(city) {
   let apiKey = "63f6b7ca37c5806f98b038dfd37c0251";
