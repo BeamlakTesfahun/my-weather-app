79b045a816dd684430aca706198de89ecff9e76a
Beamlak <beamlakt24@gmail.com>
2023-11-05 11:14:57 +0300

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function updateWeatherParameters(response) {
+  console.log(response.data);
+  let citySelector = document.querySelector("#city-id");
+  citySelector.innerHTML = response.data.name;
+  let temp = response.data.main.temp;
+  let tempSelector = document.querySelector("#temp-id");
+  console.log(tempSelector);
+  tempSelector.innerHTML = Math.round(temp);
+}
+function cityFunction(city) {
+  let apiKey = "63f6b7ca37c5806f98b038dfd37c0251";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherParameters);
+}
 function searchFormFunction(event) {
   event.preventDefault();
   let formInput = document.querySelector("#search-form-input-id");
-  let citySelector = document.querySelector("#city-id");
-  citySelector.innerHTML = formInput.value;
+
+  cityFunction(formInput.value);
 }
 let searchFormSelector = document.querySelector("#search-form-id");
 searchFormSelector.addEventListener("submit", searchFormFunction);
+cityFunction("Addis Ababa");
